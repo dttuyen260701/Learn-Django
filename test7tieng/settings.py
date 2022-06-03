3202c95ce150bb719e0d97603d88de17853de3d0
Tuyen <dttuyen260701@gmail.com>
2022-06-03 12:47:43 +0700

    T

--- test7tieng/settings.py
+++ test7tieng/settings.py
@@ -33,7 +33,7 @@ DEBUG = os.environ.get('DEBUG', False)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
